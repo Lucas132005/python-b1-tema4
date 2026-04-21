aef1205d92f2d71815e45049f1704439f2221a61
Lucas132005 <eguibo2@gmail.com>
2026-04-21 11:19:50 +0200

    Ejercicio 4 hecho

--- 4a/ej4a4.py
+++ 4a/ej4a4.py
@@ -30,6 +30,8 @@ Enunciat:
 
 
 def add_elapsed_time(dictionary, key, value):
+    dictionary[key] = value
+    return dictionary
     # Write here your code
     pass
 
@@ -37,7 +39,7 @@ def add_elapsed_time(dictionary, key, value):
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# my_dict = {"Juan": 9.5, "Peter": 14.2, "Sofia": 6.5, "Alex": 8.7}
-# my_dict = add_elapsed_time(my_dict, "Juan", 8.8)
-# my_dict = add_elapsed_time(my_dict, "Alice", 7.3)
-# print(my_dict)
+my_dict = {"Juan": 9.5, "Peter": 14.2, "Sofia": 6.5, "Alex": 8.7}
+my_dict = add_elapsed_time(my_dict, "Juan", 8.8)
+my_dict = add_elapsed_time(my_dict, "Alice", 7.3)
+print(my_dict)
